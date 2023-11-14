a6e21ad1f81b5850220a1fe593e349d62786e44e
NafisAamir <aamirnafis82@gmail.com>
2023-11-14 15:17:32 +0530

    Deployment changes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     include: ['react-redux'],
+//   },
+// })
+// vite.config.js
+import react from '@vitejs/plugin-react';
+
+export default {
   plugins: [react()],
-  optimizeDeps: {
-    include: ['react-redux'],
+  build: {
+    rollupOptions: {
+      external: ['react-redux'],
+    },
   },
-})
+};
