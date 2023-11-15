02e09810aab02aa5fb8186132dd37881ed2eb6ec
NafisAamir <aamirnafis82@gmail.com>
2023-11-15 08:54:59 +0530

    Output Component aligned

--- src/Components/Output.jsx
+++ src/Components/Output.jsx
@@ -1,28 +1,15 @@
 import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux'; // Import useDispatch
+import { useSelector, useDispatch } from 'react-redux'; 
 import axios from 'axios';
 import { setData } from '../Redux/action';
 
 const Output = () => {
-  const dispatch = useDispatch(); // Initialize useDispatch
+  const dispatch = useDispatch(); 
   const pin = useSelector((state) => state.pin);
   const data = useSelector((state) => state.data);
 
   useEffect(() => {
-    // const fetchData = async () => {
-    //   if (pin) {
-    //     try {
-    //       const response = await axios.get(`https://api.zippopotam.us/in/${pin}`);
-    //       console.log(response.data);
-    //       // Dispatch the action to set the data in the Redux store
-    //       dispatch({ type: 'SET_DATA', payload: response.data.places[0] });
-    //     } catch (error) {
-    //       console.error("Error fetching data:", error);
-    //     }
-    //   }
-    // };
-
-    // fetchData();
+    
     dispatch(setData(pin))
   }, []);
 
@@ -35,9 +22,6 @@ const Output = () => {
           <h3>Longitude: {data.longitude}</h3>
           <h3>Latitude: {data.latitude}</h3>
           
-          
-
-          {/* Add other data properties as needed */}
         </div>
       )}
     </div>
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   optimizeDeps: {
-//     include: ['react-redux'],
-//   },
-// })
-// vite.config.js
-import react from '@vitejs/plugin-react';
-
-export default {
+// https://vitejs.dev/config/
+export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-redux'],
-    },
+  optimizeDeps: {
+    include: ['react-redux'],
   },
-};
+})
+// vite.config.js
+// import react from '@vitejs/plugin-react';
+
+// export default {
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       external: ['react-redux'],
+//     },
+//   },
+// };
